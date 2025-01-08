ea1030d5c83664972869290667640022a59eca02
Kraknes <erling.heimstad.willassen@gmail.com>
2025-01-08 22:24:33 +0100

    endret 08.01.25

--- p1-pre/p1-pre/src/test.c
+++ p1-pre/p1-pre/src/test.c
@@ -51,13 +51,48 @@ size_t list_length(list_t *list) {
     return list->length;
 }
 
+void list_destroy(list_t *list, free_fn item_free) {
+    free(list);
+    free(item_free);
+    printf("list destroyed \n");
+
+}
+
+int list_addfirst(list_t *list, void *item) {
+    lnode_t *first_node = malloc(sizeof(lnode_t));
+    first_node->item = item;
+
+    if (list->length == 0) {
+        list->tail = first_node;
+        list->head = first_node;
+    }
+    else {
+        first_node->next = list->head;
+        list->head = first_node;
+    }
+
+    list->length++;
+    return 0;
+}
+
 int main(){
     printf("yo \n");
 
-    list_t *words = list_create((cmp_fn) strcmp);
-    
+    list_t *list = list_create((cmp_fn) strcmp);
+    printf("List length before nodes is: %lu \n", list->length);
+    list_addfirst(list, "hello");
+    list_addfirst(list, "world");
+    printf("List length after nodes is: %lu \n", list->length);
 
-    printf("%lu \n", words->length);
-    printf("%p \n", words->tail);
+    printf("Memory adress for head is: %p \n", list->head);
+    printf("Item for head is: %s \n", (char *)list->head->item);
+    printf("Memory adress for tail is: %p \n", list->tail);
+    printf("Item for tail si: %s \n", (char *)list->tail->item);
+
+    printf("size of list before destruction: %lu \n", sizeof(list));
+    list_destroy(list, NULL);
+    printf("size of list after destruction: %lu \n", sizeof(list));
+    // printf("Does list exist? Testing head item: %s", (char *)list->head->item);
+    printf("Does list exist?: %p \n", list);
     return 0;
 }
