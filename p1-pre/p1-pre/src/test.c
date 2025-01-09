d945993faa27f51c412295c67167289b3295da50
Kraknes <erling.heimstad.willassen@gmail.com>
2025-01-09 11:57:47 +0100

    endret 09.01.25

--- p1-pre/p1-pre/src/test.c
+++ p1-pre/p1-pre/src/test.c
@@ -34,7 +34,7 @@ struct list_iter {
 };
 
 
-
+// funker
 list_t *list_create(cmp_fn cmpfn) {
     list_t *list = malloc(sizeof(list_t));
     list->head = NULL;
@@ -43,7 +43,7 @@ list_t *list_create(cmp_fn cmpfn) {
     list->cmpfn = cmpfn;
     return list;
 }
-
+// funker, men bør ha en itererings funksjon for hver node istedenfor list->length
 size_t list_length(list_t *list) {
     if (list->length == 0) {
         return 0;
@@ -51,13 +51,16 @@ size_t list_length(list_t *list) {
     return list->length;
 }
 
+// funker kanskje? 
 void list_destroy(list_t *list, free_fn item_free) {
+
     free(list);
     free(item_free);
     printf("list destroyed \n");
 
 }
 
+// funker
 int list_addfirst(list_t *list, void *item) {
     lnode_t *first_node = malloc(sizeof(lnode_t));
     first_node->item = item;
@@ -75,19 +78,37 @@ int list_addfirst(list_t *list, void *item) {
     return 0;
 }
 
+// funker
+int list_addlast(list_t *list, void *item) {
+    lnode_t *last_node = malloc(sizeof(lnode_t));
+    last_node->item = item;
+    last_node->next = NULL;
+    list->tail->next = last_node;
+    list->tail = last_node;
+    
+    list->length++;
+    return 0;
+}
+
+
 int main(){
     printf("yo \n");
 
     list_t *list = list_create((cmp_fn) strcmp);
     printf("List length before nodes is: %lu \n", list->length);
-    list_addfirst(list, "hello");
-    list_addfirst(list, "world");
-    printf("List length after nodes is: %lu \n", list->length);
+    list_addfirst(list, "dette er første node lagt til head");
+    list_addfirst(list, "dette er andre node lagt til head");
+    printf("List length after two head nodes added is: %lu \n", list->length);
 
     printf("Memory adress for head is: %p \n", list->head);
     printf("Item for head is: %s \n", (char *)list->head->item);
+    printf("Memory adress for node nr.2 is: %p \n", list->head->next);
+    printf("Item for tail si: %s \n", (char *)list->head->next->item);
+
+    list_addlast(list, "tredje node lagt til listen til tail");
     printf("Memory adress for tail is: %p \n", list->tail);
     printf("Item for tail si: %s \n", (char *)list->tail->item);
+    printf("List length after two head nodes and one tail added is: %lu \n", list->length);
 
     printf("size of list before destruction: %lu \n", sizeof(list));
     list_destroy(list, NULL);
